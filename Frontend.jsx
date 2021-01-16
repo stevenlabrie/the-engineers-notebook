8043088a190425830582bdc44fa59a9fdcf72378
Tommy Liang <tommyliangsays@gmail.com>
2021-01-15 21:45:00 -0500

    Added file structure, created backend, frontend, sql file

---
+++ Frontend.jsx
@@ -0,0 +1,10 @@
+import React, { useState } from 'react';
+
+function Frontend() {
+  return (
+    <div className="frontend">
+      <h1>Frontend</h1>
+    </div>
+  );
+}
+export default Frontend;
---
+++ components/Backend.jsx
@@ -0,0 +1,10 @@
+import React, { useState } from 'react';
+
+function Backend() {
+  return (
+    <div className="backend">
+      <h1>Backend</h1>
+    </div>
+  );
+}
+export default Backend;
